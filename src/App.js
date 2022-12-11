4a605596d9844464acb5bfe5a3237f8593e3e9ea
NicksyRop <nicksonkipkorir25@gmail.com>
2022-12-12 02:43:05 +0300

    added spinner and edoited the header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#56ed24");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <RingLoader color={color} loading={loading} size={50} />
+      ) : (
+        <header className="App-header">
+          <article>
+            <h1>We&rsquo;ll be back soon!</h1>
+            <div>
+              <p>
+                Sorry for the inconvenience our system is under maintenance at
+                the moment. We&rsquo;ll be back online shortly!.
+              </p>
+              <p>
+                Contact :{" "}
+                <a href="mailto:info@goldengateconsultants.com">
+                  info@goldengateconsultants.com
+                </a>
+              </p>
+            </div>
+          </article>
+        </header>
+      )}
     </div>
   );
 }
